62e857ee7d64f44f662472565f5292c42e4a9cf9
Gabriel Brun <lydstyl@gmail.com>
2019-12-19 23:27:08 +0100

    REMOVE_CHARGES_LIST from db and with Flux architecture

--- src/components/ChargesLists/ChargesLists.jsx
+++ src/components/ChargesLists/ChargesLists.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
 
 import { useCharge } from '../../Reducers/useCharge';
+import { db } from '../../utils/firebase/base';
 
 const ChargesLists = () => {
   const { chargeStore, chargeDispatch } = useCharge();
 
+  const handleRemoveList = id => {
+    db.collection('users')
+      .doc(id)
+      .delete()
+      .then(function() {
+        console.log('Document successfully deleted!');
+        chargeDispatch({
+          type: 'REMOVE_CHARGES_LIST',
+          payload: id
+        });
+      })
+      .catch(function(error) {
+        console.error('Error removing document: ', error);
+      });
+  };
+
   return (
     <div>
       <p>{JSON.stringify(chargeStore)}</p>
+
+      <ul>
+        {chargeStore.map(chargeList => (
+          <li key={chargeList.id}>
+            <p>{chargeList.email}</p>
+            <button onClick={() => handleRemoveList(chargeList.id)}>DEL</button>
+          </li>
+        ))}
+      </ul>
 
       <button
         onClick={() =>
