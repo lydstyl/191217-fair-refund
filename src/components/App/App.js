cb31b416eb9000ac08db81ec17989182d4c75fb2
Gabriel Brun <lydstyl@gmail.com>
2019-12-19 23:50:39 +0100

    clear component not used anymore

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Home from '../Home/Home';
+import Menu from '../Menu/Menu';
 import Login from '../Login/Login';
 import SignUp from '../SignUp/SignUp';
-import Quotes from '../Quotes/Quotes';
-import ChargeListBox from '../ChargeListBox/ChargeListBox';
 import PrivateRoute from '../PrivateRoute/PrivateRoute';
-import Menu from '../Menu/Menu';
-
+import Home from '../Home/Home';
+import Quotes from '../Quotes/Quotes';
 import ChargesLists from '../ChargesLists/ChargesLists';
 
-import { AuthProvider } from '../Auth/Auth';
-
-import { UserCtxProvider } from '../../Reducers/useUser';
-import { SettingsCtxProvider } from '../../Reducers/useSettings';
-
-import Counter from '../Counter';
-
+import { AuthProvider } from '../Auth/Auth'; // maybe to change
+import { UserCtxProvider } from '../../Reducers/useUser'; // with this one ?
 import { ChargeCtxProvider } from '../../Reducers/useCharge';
 
 import './App.scss';
@@ -28,25 +21,21 @@ export const App = () => {
       <Router>
         <Menu />
 
-        <ChargeCtxProvider>
-          <UserCtxProvider>
-            <SettingsCtxProvider>
-              <div className='container'>
-                <Route exact path='/login' component={Login} />
-                <Route exact path='/signup' component={SignUp} />
-                <PrivateRoute exact path='/' component={Home} />
-                <PrivateRoute exact path='/quotes' component={Quotes} />
-                <PrivateRoute exact path='/test' component={ChargeListBox} />
-                <PrivateRoute exact path='/test2' component={Counter} />
-                <PrivateRoute
-                  exact
-                  path='/charges-lists'
-                  component={ChargesLists}
-                />
-              </div>
-            </SettingsCtxProvider>
-          </UserCtxProvider>
-        </ChargeCtxProvider>
+        <UserCtxProvider>
+          <ChargeCtxProvider>
+            <div className='container'>
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/signup' component={SignUp} />
+              <PrivateRoute exact path='/' component={Home} />
+              <PrivateRoute exact path='/quotes' component={Quotes} />
+              <PrivateRoute
+                exact
+                path='/charges-lists'
+                component={ChargesLists}
+              />
+            </div>
+          </ChargeCtxProvider>
+        </UserCtxProvider>
 
         <footer>
           <div>
