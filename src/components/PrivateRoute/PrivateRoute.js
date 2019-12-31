54ed00b3f2c8656e828e5cd530a0557d28f86156
Gabriel Brun <lydstyl@gmail.com>
2019-12-31 18:29:49 +0100

    userStore instead of AuthContext in PrivateRoute

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-import { AuthContext } from '../Auth/Auth';
+import { useUser } from '../../reducers/useUser';
 
 const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
-  const { currentUser } = useContext(AuthContext);
+  const { userStore } = useUser();
+  const currentUser = userStore.currentUser;
 
   return (
     <Route
